bcd439be3f7f2adba7649cbd445412f59ac5b199
Yuri Numerov <achi@hush.ai>
2016-05-29 03:33:50 +0200

    streaming works

--- twitter-stream-in.py
+++ twitter-stream-in.py
@@ -1,44 +1,58 @@
 #! /usr/bin/env python
 
-import tweepy
+import json, os, tweepy
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
-import apikeys
-import windows
+import apikeys, windows
 
-# auth = OAuthHandler(apikeys.apikey, apikeys.apisecret)
-# auth.set_access_token(apikeys.consumerkey, apikeys.consumersecret)
-# api = tweepy.API(auth)
+auth = OAuthHandler(apikeys.apikey, apikeys.apisecret)
+auth.set_access_token(apikeys.consumerkey, apikeys.consumersecret)
+api = tweepy.API(auth)
+w=windows.update_window()
+w.send(None)
 
 class listener(StreamListener):
 
   def on_data(self, data):
     try:
+
+      data=json.loads(data)
+
       text=data["text"]
       user="%s (@%s)"%(data["user"]["name"],data["user"]["screen_name"])
       date=data["created_at"]
-      avatar=data["user"]["profile_image_url"].replace("\/","/")
 
-      updatescreen(date,user,text,avatar)
+      # Get image
+      # os.system("rm avatar")
+      # avatar=data["user"]["profile_image_url"].replace("\/","/")
+      # zz=os.system("wget %s -o avatar 2> /dev/null"%avatar)
+      # avatar="avatar" if not zz else "init.jpg"
+
+      w.send((date,user,text))#,avatar))
+
     except BaseException as e:
+
       with open('./log', 'a') as f:
-        print("\n----------------------\nError on_data: %s\n" % str(e))
+        print("\n----------------------\nError on_data: %s" % str(e))
+
     finally:
+
       return True
 
   def on_error(self, status):
     print(status)
     return True
 
-# twitter_stream = Stream(auth, listener())
-# twitter_stream.filter(track=['#GasteizMakerDay'])
+twitter_stream = Stream(auth, listener())
+twitter_stream.filter(track=['#followback'])
 # 
 
-testdata=[["2015-20-20 :: 13:37:04", "@blaubleublue", "Complex system replacements feel like like they'll be simpler and faster to build. But that is because human brains can only hold cartoons.", "avatar1"], ["date2", "user2", "Complex system replacements feel like like they'll be simpler and faster to build. But that is because human brains can only hold cartoons.", "avatar2"],["date3", "user3", "Complex system replacements feel like like they'll be simpler and faster to build. But that is because human brains can only hold cartoons.", "avatar3"], ["date4", "user4", "Complex system replacements feel like like they'll be simpler and faster to build. But that is because human brains can only hold cartoons.", "avatar4"]]
+# testdata=[["2015-20-20 :: 13:37:04", "@blaubleublue", "Complex system replacements feel like like they'll be simpler and faster to build. But that is because human brains can only hold cartoons.", "avatar1"], 
+# ["2015-20-20 :: 13:37:10", "@trex", "buy my new shirt, 'i object to the use of the word waifu, but i think you are welcome to like any anime cutie you want'", "avatar2"],
+# ["2015-20-20 :: 13:39:11", "@amazewalls", "Coroutines and generators are awesome", "avatar3"], 
+# ["2015-20-20 :: 13:47:44", "@totallynotabot", "Beautiful new large-scale brain etchings from the fantastic Gregg Dun thepipetteer.com/self-reflected... pic.twitter.com/SNFkSgoUkw", "avatar4"]]
 
-w=windows.update_window()
-w.send(None)
-for i in testdata: 
-  w.send(i)
-  import time; time.sleep(3)
+# for i in testdata: 
+#   w.send(i)
+#   import time; time.sleep(10)
--- windows.py
+++ windows.py
@@ -1,21 +1,25 @@
 #! /usr/bin/env python
 
 import tkFont, Tkinter
-
+from PIL import Image, ImageTk
 
 # Windows is created when importing this module
 root=Tkinter.Tk()
 root.wm_title("PySkeleton")
 bigfont=tkFont.Font(root=root, font=None, name=None, family='Mono', size=50, weight='bold')
-bigishfont=tkFont.Font(root=root, font=None, name=None, family='Mono', size=30, weight='bold')
-smallfont=tkFont.Font(root=root, font=None, name=None, family='Mono', size=25, weight='bold')
+bigishfont=tkFont.Font(root=root, font=None, name=None, family='Mono', size=26, weight='bold')
+smallfont=tkFont.Font(root=root, font=None, name=None, family='Mono', size=25)
 Tkinter.mainloop(1) 
 
 def update_window():
 
+  # defaultimg=ImageTk.PhotoImage(Image.open("./init.jpg"))
+  # photo=Tkinter.Label(image=defaultimg, width=100, height=100)
+  # photo.image=defaultimg
   name=Tkinter.Label(text="", font=bigfont)
   date=Tkinter.Label(text="", font=bigishfont)
   tweet=Tkinter.Label(text="", font=smallfont, wraplength=800, justify="left")
+  # photo.pack()
   name.pack(side="top", padx=10, fill="x")
   date.pack(side="top", padx=10, fill="x")
   tweet.pack(side="top", padx=10, fill="x")
@@ -23,10 +27,14 @@ def update_window():
 
   while 1:
 
-    timestamp,user,text,avatar = yield
-    name.config(text=user+"\n")
+    timestamp,user,text = yield #, avatar
+    name.config(text=user)
     date.config(text=timestamp+"\n")
     tweet.config(text=text)
+    # try:
+    #   photo.config(image=Image.open(avatar))
+    # except: 
+    #   photo.config(image=defaultimg)
     root.update()
     
  
